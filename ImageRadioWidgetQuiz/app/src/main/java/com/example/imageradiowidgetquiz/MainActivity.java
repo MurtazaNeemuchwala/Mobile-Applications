4188a601e95ef076c36087e4417c4b01af0ad044
MurtazaNeemuchwala <mneemuch914@gmail.com>
2020-11-16 13:04:52 -0500

    ...

---
+++ ImageRadioWidgetQuiz/app/src/main/java/com/example/imageradiowidgetquiz/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.imageradiowidgetquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ImageView imageView;
+    RadioGroup radioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
