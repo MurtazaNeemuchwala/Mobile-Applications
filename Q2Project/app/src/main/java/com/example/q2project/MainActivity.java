ab0b685fb1a11770482cb2732ea72710c291d2e8
MurtazaNeemuchwala <mneemuch914@gmail.com>
2020-12-14 14:59:49 -0500

    stuf

--- Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
+++ Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
@@ -6,13 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -33,19 +36,24 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.id_listview_main);
         arrayList = new ArrayList<>();
 
-        listViewAdapter adapter = new listViewAdapter(this, R.layout.adapter_listview, arrayList);
+        CustomAdapter adapter = new CustomAdapter(this, R.layout.adapter_listview, arrayList);
         listView.setAdapter(adapter);
 
-        arrayList.add(new Heros("Batman", "DC Universe, no super powers, but, is a master engineer and one heck of a fighter",(R.drawable.batman)));
+        arrayList.add(new Heros("Batman", "DC Universe.", (R.drawable.batman)));
+        arrayList.add(new Heros("Captain America", "Marvel Universe.", (R.drawable.captain_america)));
+        arrayList.add(new Heros("Spiderman", "Marvel Universe.", (R.drawable.spiderman)));
+        arrayList.add(new Heros("Ironman", "Marvel Universe.", (R.drawable.ironman)));
+        arrayList.add(new Heros("Hulk", "Marvel Universe.", (R.drawable.hulk)));
+        arrayList.add(new Heros("Flash", "DC Universe.", (R.drawable.flash)));
     }
 
-    public class listViewAdapter extends ArrayAdapter<Heros>{
+
+    public class CustomAdapter extends ArrayAdapter<Heros> {
         List<Heros> list;
         Context context;
         int xml;
 
-
-        public listViewAdapter(@NonNull Context context, int resource, @NonNull List<Heros> objects) {
+        public CustomAdapter(@NonNull Context context, int resource, @NonNull List<Heros> objects) {
             super(context, resource, objects);
             this.context = context;
             xml = resource;
@@ -59,20 +67,19 @@ public class MainActivity extends AppCompatActivity {
             Button button = adapterLayout.findViewById(R.id.id_adapter_button);
             ImageView picture = adapterLayout.findViewById(R.id.id_adapter_imageView);
 
-            textView.setText( list.get(position).getName());
+            textView.setText(list.get(position).getName());
             button.setText("REMOVE");
             picture.setImageResource(list.get(position).getImage());
             return adapterLayout;
-
         }
     }
 
-    public class Heros{
+    public class Heros {
         String name;
         String description;
         int image;
 
-        public Heros(String name, String description, int image){
+        public Heros(String name, String description, int image) {
             this.name = name;
             this.description = description;
             this.image = image;
