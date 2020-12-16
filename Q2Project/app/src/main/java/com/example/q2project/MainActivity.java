453f3109cdc54bfeec54e0cb1612c6ff2e2e586f
MurtazaNeemuchwala <MurtazaNeemuchwala@users.noreply.github.com>
2020-12-16 15:50:59 -0500

    mneemuch914@gmail.com

--- Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
+++ Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<Heros> arrayList;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +38,14 @@ public class MainActivity extends AppCompatActivity {
         CustomAdapter adapter = new CustomAdapter(this, R.layout.adapter_listview, arrayList);
         listView.setAdapter(adapter);
 
-        arrayList.add(new Heros("Batman", "DC Universe.", (R.drawable.batman)));
-        arrayList.add(new Heros("Captain America", "Marvel Universe.", (R.drawable.captain_america)));
-        arrayList.add(new Heros("Spiderman", "Marvel Universe.", (R.drawable.spiderman)));
-        arrayList.add(new Heros("Ironman", "Marvel Universe.", (R.drawable.ironman)));
-        arrayList.add(new Heros("Hulk", "Marvel Universe.", (R.drawable.hulk)));
-        arrayList.add(new Heros("Flash", "DC Universe.", (R.drawable.flash)));
-    }
+        arrayList.add(new Heros("Batman", "DC Universe. His abilities include above human intelligence, access to advanced technologies, adept hand to hand combat, and close to peak human capabilities", (R.drawable.batman)));
+        arrayList.add(new Heros("Captain America", "Marvel Universe. His abilities include superhuman capabilities, mastery in all forms of martial arts, and dexterity with his vibranium crafted shield.", (R.drawable.captain_america)));
+        arrayList.add(new Heros("Spiderman", "Marvel Universe. Spiderman uses his genius level mind and superhuman strength and stamina to fight off crime in the city of New York.", (R.drawable.spiderman)));
+        arrayList.add(new Heros("Ironman", "Marvel Universe. His abilities include above human intelligence, access to advanced technologies, and phenomenal engineering skills.", (R.drawable.ironman)));
+        arrayList.add(new Heros("Hulk", "Marvel Universe. His abilities include, above human intelligence, superior knowledge in the field of chemistry and physics, and can become an enraged super human when angered. ", (R.drawable.hulk)));
+        arrayList.add(new Heros("Flash", "DC Universe. His abilities include, above human intelligence, superhuman speed which allows him to asses and resolve critical situation in the matter of a split second.", (R.drawable.flash)));
 
+    }
 
     public class CustomAdapter extends ArrayAdapter<Heros> {
         List<Heros> list;
@@ -63,13 +62,36 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             View adapterLayout = inflater.inflate(xml, null);
+
             TextView textView = adapterLayout.findViewById(R.id.id_adapter_TextView);
             Button button = adapterLayout.findViewById(R.id.id_adapter_button);
             ImageView picture = adapterLayout.findViewById(R.id.id_adapter_imageView);
 
-            textView.setText(list.get(position).getName());
-            button.setText("REMOVE");
-            picture.setImageResource(list.get(position).getImage());
+            adapterLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    remove(list.get(position));
+
+                }
+            });
+
+            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+                textView.setText(list.get(position).getName());
+                button.setText("REMOVE");
+                picture.setImageResource(list.get(position).getImage());
+            }
+            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                textView.setText(list.get(position).getDescription());
+                button.setText("REMOVE");
+                picture.setImageResource(list.get(position).getImage());
+            }
             return adapterLayout;
         }
     }
