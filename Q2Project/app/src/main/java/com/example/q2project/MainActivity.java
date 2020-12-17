254fd99041353ac844bf98dc23085e8b7e679311
MurtazaNeemuchwala <MurtazaNeemuchwala@users.noreply.github.com>
2020-12-17 18:20:46 -0500

    finished
    Saved state has been overrided and orientation switches dont clear selections

--- Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
+++ Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,28 +28,72 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     ArrayList<Heros> arrayList;
+    TextView name, description;
+    Button remove;
+    ImageView image;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("KEY_LIST", arrayList);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        arrayList = (ArrayList<Heros>) savedInstanceState.getSerializable("KEY_LIST");
+    }
+
+   /* @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.id_listview_main);
+        arrayList = new ArrayList<>();
 
-        if(savedInstanceState == null) {
-            arrayList = new ArrayList<>();
+        arrayList.add(new Heros("Batman", "DC Universe. His abilities include above human intelligence, access to advanced technologies, adept hand to hand combat, and close to peak human capabilities", (R.drawable.batman)));
+        arrayList.add(new Heros("Captain America", "Marvel Universe. His abilities include superhuman capabilities, mastery in all forms of martial arts, and dexterity with his vibranium crafted shield.", (R.drawable.captain_america)));
+        arrayList.add(new Heros("Spiderman", "Marvel Universe. Spiderman uses his genius level mind and superhuman strength and stamina to fight off crime in the city of New York.", (R.drawable.spiderman)));
+        arrayList.add(new Heros("Ironman", "Marvel Universe. His abilities include above human intelligence, access to advanced technologies, and phenomenal engineering skills.", (R.drawable.ironman)));
+        arrayList.add(new Heros("Hulk", "Marvel Universe. His abilities include, above human intelligence, superior knowledge in the field of chemistry and physics, and can become an enraged super human when angered. ", (R.drawable.hulk)));
+        arrayList.add(new Heros("Flash", "DC Universe. His abilities include, above human intelligence, superhuman speed which allows him to asses and resolve critical situation in the matter of a split second.", (R.drawable.flash)));
 
-            arrayList.add(new Heros("Batman", "DC Universe. His abilities include above human intelligence, access to advanced technologies, adept hand to hand combat, and close to peak human capabilities", (R.drawable.batman)));
-            arrayList.add(new Heros("Captain America", "Marvel Universe. His abilities include superhuman capabilities, mastery in all forms of martial arts, and dexterity with his vibranium crafted shield.", (R.drawable.captain_america)));
-            arrayList.add(new Heros("Spiderman", "Marvel Universe. Spiderman uses his genius level mind and superhuman strength and stamina to fight off crime in the city of New York.", (R.drawable.spiderman)));
-            arrayList.add(new Heros("Ironman", "Marvel Universe. His abilities include above human intelligence, access to advanced technologies, and phenomenal engineering skills.", (R.drawable.ironman)));
-            arrayList.add(new Heros("Hulk", "Marvel Universe. His abilities include, above human intelligence, superior knowledge in the field of chemistry and physics, and can become an enraged super human when angered. ", (R.drawable.hulk)));
-            arrayList.add(new Heros("Flash", "DC Universe. His abilities include, above human intelligence, superhuman speed which allows him to asses and resolve critical situation in the matter of a split second.", (R.drawable.flash)));
-
-            CustomAdapter adapter = new CustomAdapter(this, R.layout.adapter_listview, arrayList);
-            listView.setAdapter(adapter);
-        }else{
-            //arrayList = savedInstanceState.getParcelableArrayList(SAVESTATE_COUNT);
+        if(savedInstanceState!=null) {
+            arrayList = (ArrayList<Heros>) savedInstanceState.getSerializable("KEY_LIST");
         }
+        final CustomAdapter adapter = new CustomAdapter(this, R.layout.adapter_listview, arrayList);
+        listView.setAdapter(adapter);
     }
 
     public class CustomAdapter extends ArrayAdapter<Heros> {
@@ -86,12 +131,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
                 textView.setText(list.get(position).getName());
                 button.setText("REMOVE");
                 picture.setImageResource(list.get(position).getImage());
             }
-            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 textView.setText(list.get(position).getDescription());
                 button.setText("REMOVE");
                 picture.setImageResource(list.get(position).getImage());
@@ -124,4 +169,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
 }
