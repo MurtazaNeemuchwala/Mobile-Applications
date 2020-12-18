420567f3718078ca2b5ded50419c5b349363606b
MurtazaNeemuchwala <MurtazaNeemuchwala@users.noreply.github.com>
2020-12-18 15:00:23 -0500

    beautify UI fix small bugs

--- Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
+++ Q2Project/app/src/main/java/com/example/q2project/MainActivity.java
@@ -82,14 +82,42 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.id_listview_main);
         arrayList = new ArrayList<>();
 
-        arrayList.add(new Heros("Batman", "DC Universe. His abilities include above human intelligence, access to advanced technologies, adept hand to hand combat, and close to peak human capabilities", (R.drawable.batman)));
-        arrayList.add(new Heros("Captain America", "Marvel Universe. His abilities include superhuman capabilities, mastery in all forms of martial arts, and dexterity with his vibranium crafted shield.", (R.drawable.captain_america)));
-        arrayList.add(new Heros("Spiderman", "Marvel Universe. Spiderman uses his genius level mind and superhuman strength and stamina to fight off crime in the city of New York.", (R.drawable.spiderman)));
-        arrayList.add(new Heros("Ironman", "Marvel Universe. His abilities include above human intelligence, access to advanced technologies, and phenomenal engineering skills.", (R.drawable.ironman)));
-        arrayList.add(new Heros("Hulk", "Marvel Universe. His abilities include, above human intelligence, superior knowledge in the field of chemistry and physics, and can become an enraged super human when angered. ", (R.drawable.hulk)));
-        arrayList.add(new Heros("Flash", "DC Universe. His abilities include, above human intelligence, superhuman speed which allows him to asses and resolve critical situation in the matter of a split second.", (R.drawable.flash)));
+        //Batman
+        arrayList.add(new Heros(
+                "Batman",
+                "Hails from DC Universe. His whole story started when he witnessed his parents getting murdered in a robbery gone wrong. Ever since that moment in his life he pledged that he would make it his job to end crime in Gotham City. With the help of his team which includes, Alfred, Lucius Fox, and Oracle he plans to take down every Villain he can.",
+                (R.drawable.batman)));
 
-        if(savedInstanceState!=null) {
+        //Captain America
+        arrayList.add(new Heros(
+                "Captain America",
+                "Hails from Marvel Universe. Steve Rodgers was just a teenager drafted by the army to fight in WWII. The army started conducting special experiments to create biological Super-Humans so he volunteered and the experiment was a success. He was given super human abilities which made him unstoppable. He takes on Hydra to defeat them once and for all but in the process he is forced to sacrifice his life to save a city from a bombing. Captain does the ultimate deed and nose dives the plane with his body in it.",
+                (R.drawable.captain_america)));
+
+        //Spidy
+        arrayList.add(new Heros(
+                "Spiderman",
+                "Hails from Marvel Universe. Peter Parker was on a school field trip to a laboratory, Peter was wandering around and found a radioactive spider and got bit by it. He started to notice there were some changes happening to him, he had strong grip, great strength and high jumping abilities. He started to create web slingers and a suit to become his new persona, \"spiderman\".",
+                (R.drawable.spiderman)));
+
+        //Ironman
+        arrayList.add(new Heros(
+                "Ironman",
+                "Hails from Marvel Universe. Tony Stark, a tech wiz, a millionaire. He was once very arrogant a very egotistical person until he faced the hardest moments in his life. Ironman was held hostage by a terror group, he knew the only was he'd survive is if he escapes. In a cave Tony made a manned robot which he would use to fight his way out. He then realised his true mission in life, he must eradicate all terrorists in the world and make world peace.",
+                (R.drawable.ironman)));
+
+        //Hulk
+        arrayList.add(new Heros("Hulk",
+                "Hails from Marvel Universe. Bruce Banner a physicist, a chemist, and a biologist was conducting an experiment where he was hit by gamma rays and radiation. The radiation was so strong that the human biology in him was altered. Bruce had become a brute. He had become the \"Hulk\".",
+                (R.drawable.hulk)));
+
+        //Flash
+        arrayList.add(new Heros(
+                "Flash",
+                "Hails from DC Universe. Barry Allen a detective was working a case and during an  investigation he was struck by lighting due to a particle accelerator. This put him in coma for several years. Once he finally woke up he had felt more energetic than ever, it was almost like time around him had slowed down. He then realised that the particle accelerator had caused a biological change in his body, his body and brain was actually moving faster. Upon more experimentation he had found out that he had super human speed.",
+                (R.drawable.flash)));
+
+        if (savedInstanceState != null) {
             arrayList = (ArrayList<Heros>) savedInstanceState.getSerializable("KEY_LIST");
         }
         final CustomAdapter adapter = new CustomAdapter(this, R.layout.adapter_listview, arrayList);
@@ -113,33 +141,37 @@ public class MainActivity extends AppCompatActivity {
             View adapterLayout = inflater.inflate(xml, null);
 
             TextView textView = adapterLayout.findViewById(R.id.id_adapter_TextView);
+            TextView description = adapterLayout.findViewById(R.id.textView_description);
             Button button = adapterLayout.findViewById(R.id.id_adapter_button);
             ImageView picture = adapterLayout.findViewById(R.id.id_adapter_imageView);
-
-            adapterLayout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                }
-            });
 
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     remove(list.get(position));
-
                 }
             });
+
+
 
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
                 textView.setText(list.get(position).getName());
                 button.setText("REMOVE");
                 picture.setImageResource(list.get(position).getImage());
+
+
             }
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                textView.setText(list.get(position).getDescription());
+                textView.setText(list.get(position).getName());
                 button.setText("REMOVE");
                 picture.setImageResource(list.get(position).getImage());
+                adapterLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        description.setText(list.get(position).getDescription());
+                    }
+                });
+
             }
             return adapterLayout;
         }
