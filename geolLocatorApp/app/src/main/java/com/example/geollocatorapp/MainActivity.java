a123e14c0ffa4c06332c7da52a9e929cdb720cdd
Murtaza Neemuchwala <53874800+MurtazaNeemuchwala@users.noreply.github.com>
2021-02-08 08:51:56 -0500

    h

--- geolLocatorApp/app/src/main/java/com/example/geollocatorapp/MainActivity.java
+++ geolLocatorApp/app/src/main/java/com/example/geollocatorapp/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.geollocatorapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -15,45 +14,70 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
+
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
-
-    Button goButton;
-    TextView locationText;
-    Context context;
+    TextView location_textView, distance_textView;
+    Button getDistance_Button;
     LocationManager locationManager;
 
+    ArrayList<Location> coord = new ArrayList<Location>();
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        location_textView = findViewById(R.id.location_textVIew);
+        distance_textView = findViewById(R.id.distance_TextView);
+        getDistance_Button = findViewById(R.id.getDistance_button);
 
-        locationText = findViewById(R.id.location_textVIew);
-        goButton = findViewById(R.id.button_getLocation);
+        getDistance_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    distance_textView.setText(String.valueOf(coord.get(0).distanceTo(coord.get(1))));
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
 
-
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION
             }, 100);
         }
 
-        goButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getLocation();
-            }
-        });
+        getLocation();
+
     }
 
+   /* public double GetDistanceFromLatLong(double lat1, double lon1, double lat2, double lon2) {
+        final int R = 6371;
+        // Radius of the earth in km
+        double dLat = deg2rad(lat2 - lat1);
+        // deg2rad below
+        double dLon = deg2rad(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        // Distance in km
+        return d * 0.621;
+    }
+
+    public double deg2rad(double deg) {
+        return deg * (Math.PI / 180);
+    }*/
 
     private void getLocation() {
 
@@ -70,38 +94,62 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 return;
             }
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 5, MainActivity.this);
-        }catch (Exception e ){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    getLocation();
+                }  else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return;
+        }
+        // Other 'case' lines to check for other
+        // permissions this app might request.
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
         try {
-            Geocoder geocoder = new Geocoder(MainActivity.this,Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-            String address = addresses.get(0).getAddressLine(0);
+            Log.d("Murtaza", String.valueOf(location.getLatitude()));
+            coord.add(location);
 
-            locationText.setText(address);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+            Log.d("Murtaza", e.toString());
         }
 
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras){
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
     @Override
-    public void onProviderEnabled(String provider){
+    public void onProviderEnabled(String provider) {
 
     }
 
     @Override
-    public void onProviderDisabled(String provider){
+    public void onProviderDisabled(String provider) {
 
     }
 
